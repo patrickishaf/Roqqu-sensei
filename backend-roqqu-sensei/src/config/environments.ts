ba11cd5e90fa976e520dfbbcce5275daa5154e37
oscarnovember <ndukwe.peter@roqqu.com>
2024-11-23 02:17:06 +0100

    correct config setup

--- backend-roqqu-sensei/src/config/environments.ts
+++ backend-roqqu-sensei/src/config/environments.ts
@@ -11,3 +11,21 @@ export const devConfig: AppConfig = {
 export const testConfig: AppConfig = {
   dbUrl: process.env.DB_URL_TEST ?? "",
 }
+
+export const prodConfig: AppConfig = {
+  dbUrl: process.env.DB_URL ?? ""
+}
+
+export const pickConfigurationFromEnvironment = (): AppConfig => {
+  const configForEnvironment = {
+    development: devConfig,
+    production: devConfig,
+    test: testConfig,
+  }
+  const environment = process.env.NODE_ENV || 'development';
+  return configForEnvironment[environment as keyof typeof configForEnvironment];
+}
+
+export const loadConfigFromEnvironment = () => {
+  // TODO: Read the right .env file and load the configs in it
+}
--- backend-roqqu-sensei/src/config/index.ts
+++ backend-roqqu-sensei/src/config/index.ts
@@ -1,18 +1,7 @@
-import {devConfig} from "./environments";
-
-const pickConfigurationFromEnvironment = () => {
-  const configForEnvironment = {
-    development: devConfig,
-    production: devConfig,
-    test: devConfig,
-  }
-  const environment = process.env.NODE_ENV || 'development';
-  return configForEnvironment[environment as keyof typeof configForEnvironment];
-}
+import {pickConfigurationFromEnvironment} from "./environments";
 
 const config = pickConfigurationFromEnvironment();
 
 console.log({ env: process.env.NODE_ENV, config });
 
 export default config;
-
